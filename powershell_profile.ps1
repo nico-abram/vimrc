5b88fe0fc202aede85737685878b368f19656d24
unknown <abramlujan@gmail.com>
2022-04-24 21:32:05 -0300

    pwsh prof

--- powershell_profile.ps1
+++ powershell_profile.ps1
@@ -15,3 +15,15 @@ function Invoke-Environment {
 
 }
 Invoke-Environment "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
+
+Import-Module PSReadLine
+
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -PredictionViewStyle ListView
+
+# Slooooooow
+#Import-Module oh-my-posh
+#Import-Module posh-git
+
+Remove-Alias ls
+function ls { lsd -l @args }
